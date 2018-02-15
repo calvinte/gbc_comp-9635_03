a761e97ac305b1735d3b484b7a2f55283642e064
Jo60 <Jo60@users.noreply.github.com>
2018-02-15 14:23:37 -0500

    week_3 homework

---
+++ week_3/Osman-Jibril/src/index.js
@@ -0,0 +1,52 @@
+google.maps.event.addDomListener(window, 'load', function() {
+   var mapEl = document.getElementById('places-map')
+   console.log(mapEl)
+   var latLng = new google.maps.LatLng(0, 0)
+   var mapOptions = {
+     center: latLng,
+     zoom: 5,
+   }
+ 
+   var map = new google.maps.Map(mapEl, mapOptions)
+
+   var myPlaces = [{
+     content: 'Richmond Hill elementary school. The elementary school I attended.',
+     lat: 43.862494,  
+     lng: -79.405990,
+   }, {
+     content: 'New South Wales, Australia. I dont know any restaurant of note here just thought it would be nice to visit.',
+     lat: -33.847927,
+     lng: 150.953905,
+   }, {
+     content: '  Best Tacos outside Mexico, would like to try it. 420 W 15th St, New York, NY 10011, USA',
+     lat: 40.742130,
+     lng: -74.006090,
+   },{
+     content: 'Highest rated restaurant in the UK but will probably never try it. Not a fan of Scottish cuisine. Byres Rd, Glasgow G11 5RD, UK',
+     lat: 55.870865,
+     lng: -4.298876,
+   },{
+     content: 'Alo. Best rated restaurant in Toronto, would like to try it. 380 Queen St W Toronto, ON M5V',
+     lat: 43.648830,
+     lng: -79.396010,
+   }
+   ]
+ 
+   myPlaces.forEach(function(place) {
+     console.log(place.content)
+     var latLng = new google.maps.LatLng(place.lat, place.lng)
+     var marker = new google.maps.Marker({
+       map: map,
+       position: latLng,
+     })
+     var infowindow = new google.maps.InfoWindow({
+       content: place.content,
+     })
+ 
+     google.maps.event.addListener(marker, 'click', function ClickHandler() {
+       map.setCenter(marker.position)
+       map.setZoom(12)
+       infowindow.open(map, marker)
+     })
+   })
+ })
