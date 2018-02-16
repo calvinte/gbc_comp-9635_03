049ec48c85ab04f535beb0f0a58aebae01986122
Graphic <graphic@SOD-SJH-517-04M.local>
2018-02-15 19:32:23 -0500

    Week 4

--- week_4/Thoo-Rachel/src/script.js
+++ week_4/Thoo-Rachel/src/script.js
@@ -7,10 +7,22 @@ $.ajax({
 	url:'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
 	success:function(response) {
 		//can be any name ^
-		console.log(response)
 		response.response.venues.forEach(function(venue) {
-			var html = 'test venue'
+			console.log(venue)
+			var location = venue.location
+			var html = '<p>'
+			html += venue.name
+			html +='<br />'
+			html += '<img src="'
+			html += 'https://maps.googleapis.com/maps/api/staticmap'
+			html += '?zoom=15'
+			html += '&size=600x450'
+			html += '&maptype=roadmap'
+			html += '&center=' + location.lat +',' + location.lng
+			html +='">'
+			html += '</p>'
 			document.body.innerHTML+= html
+		// javascript maps take time to load and develop, but image maps aare faster but not interactive
 		})
 	},
 })
