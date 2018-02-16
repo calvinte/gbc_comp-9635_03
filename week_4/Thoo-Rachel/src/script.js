26d3409b691069b49dbef29598bfc9a1d0151a6c
Graphic <graphic@SOD-SJH-517-04M.local>
2018-02-15 20:01:33 -0500

    Week 4

--- week_4/Thoo-Rachel/src/script.js
+++ week_4/Thoo-Rachel/src/script.js
@@ -4,7 +4,7 @@ $.ajax({
 	dataType:"jsonp",
 	//jquery ajax data types
 	cache:false,
-	url:'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=dance',
+	url:'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=dance center',
 	success:function(response) {
 		//can be any name ^
 		response.response.venues.forEach(function(venue) {
