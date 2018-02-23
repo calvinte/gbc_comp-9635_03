02467ed1bbb715434e0ed26c80ad943331cde404
Graphic <graphic@SOD-SJH-517-12M.local>
2018-02-22 19:47:30 -0500

    Week 5

--- week_5/Thoo-Rachel/src/script.js
+++ week_5/Thoo-Rachel/src/script.js
@@ -12,6 +12,13 @@ $(document).ready(function(){
 		cache: false,
 		success:function(response){
 			console.log (response)
+			response.response.venues.forEach(function(venue){
+				var latLng = new google.maps.LatLng(venue.location.lat,venue.location.lng)
+				var marker = new google.maps.Marker({
+					map:map,
+					position: latLng,
+				})
+			})
 		},
 		type:'GET',
 		url:'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
