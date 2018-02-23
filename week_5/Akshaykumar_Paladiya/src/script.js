c7dd49feb535e146694cfc40a6da6157eb52f741
Graphic <graphic@SOD-SJH-517-14M.local>
2018-02-22 20:06:36 -0500

    classwork_week5
    In-class work

---
+++ week_5/Akshaykumar_Paladiya/src/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	var latlng = new google.maps.LatLng('43.730610', '-77.0060');
+	var map = new google.maps.Map(document.getElementById('foursquare-map'), {
+		center: latlng,
+		zoom: 10
+	});
+
+	$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		cache: false,
+        url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
+		success: function(response) {
+            console.log(response);
+            response.response.venues.forEach(function(venue){
+                var vanuelatlang = new google.maps.LatLng(venue.location.lat, venue.location.lng);
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position:vanuelatlang,
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: venue.name,
+                })
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.setZoom(12);
+                    infowindow.open(map, marker);
+                });      
+                google.maps.event.addListener(infowindow, 'closeclick', function() {
+                    map.panTo(this.getPosition());
+                    map.setZoom(5);
+                }); 
+            });
+        }
+
+    });
+})
+
+
