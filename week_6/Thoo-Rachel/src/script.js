a6939ae0395d69891398905b7909af3f8fb9587f
rwthoo <rwthoo@gmail.com>
2018-03-01 19:19:37 -0500

    week 6

--- week_6/Thoo-Rachel/src/script.js
+++ week_6/Thoo-Rachel/src/script.js
@@ -1,43 +1,43 @@
-var map;
-var markers = [];
-
-// initialize the map and listen for any clicks to the search button
 $(document).ready(function() {
-  initializeMap();
-
-  $('#map-input button').click(function() {
-    var term = $('#map-input input').val();
-    loadMarkersFromTerm(term);
-  });
-});
-
-// function to initialize the map to be seen on the screen
-function initializeMap() {
-  var toronto = new google.maps.LatLng('43.6532', '-79.3832');
-  map = new google.maps.Map(document.getElementById('foursquare-map'), {
-    center: toronto,
+  var latlng = new google.maps.LatLng('43.6532', '-79.3832');
+  var map = new google.maps.Map(document.getElementById('foursquare-map'), {
+    center: latlng,
     zoom: 10
   });
-}
+var markers = {};
 
-// load the markers from the term in the input box
-function loadMarkersFromTerm(term) {
-  var url = 'https://api.foursquare.com/v2/venues/search?client_id=XUXLO0NUOU5C54RCWE2UXF50MHWEL52I1APLQVSMBQJDGFPX&client_secret=0BEDRXQL312RAUAK2M5ON35BLUWSBRNCQDQNKADJ5COXJ4VJ&v=20180212&near=Toronto&query=' + term;
-
+$('mybutton').click(function(){
+  var term = $('#myinput').val();
   $.ajax({
     type: "GET",
     dataType: "jsonp",
     cache: false,
-    url: url,
-    success: function (response) {
-      clearMap();
+    url: 'https://api.foursquare.com/v2/venues/search?client_id=XUXLO0NUOU5C54RCWE2UXF50MHWEL52I1APLQVSMBQJDGFPX&client_secret=0BEDRXQL312RAUAK2M5ON35BLUWSBRNCQDQNKADJ5COXJ4VJ&v=20180212&near=Toronto&query=' + term,
+    success: function(response) {
+      //clear the map first
+      clearTheMap();
       response.response.venues.forEach(function(venue) {
-        addVenueMarkerOnMap(venue);
+        var location = venue.location
+        var latlng = new google.maps.LatLng(location.lat, location.lng);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        markers.push(marker)
+        var infowindow = new google.maps.InfoWindow({ content: venue.name });
+        google.maps.event.addListener(marker, 'click', function() {
+          map.setZoom(12);
+          infowindow.open(map, marker);
+        });
       });
     }
-  })
-}
+  });
+});
 
-$('mybutton').click(function(){
-  console.log('this was clicked');
-})
+function clearTheMap(){
+for(var i =0; i<markers.length; i++){
+  markers [i].setMap(null);
+}
+markers = [];
+}
+});
