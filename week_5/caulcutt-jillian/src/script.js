95b37b87f1b8a7bfc99bf222e3d74d27f06b767e
Jill <jillcaulcutt@hotmail.com>
2018-03-01 19:51:29 -0500

    Week 5 Final
    week 6 in class progress

--- week_5/caulcutt-jillian/src/script.js
+++ week_5/caulcutt-jillian/src/script.js
@@ -8,41 +8,52 @@ console.log('testing');
     var element = "";
  //   var map = "";
     var infoWindow = "";
+    var myCurrentPosition = "";
+    var online = navigator.onLine;
+
+    var map = "";
+    var options = "";
 
 
     $(document).ready(function() {
 
       console.log("inside map function");
+      
+
+
+
+    if (online) {
+      console.log("you're online");
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        myCurrentPosition = new google.maps.LatLng(pos);
         element = document.getElementById('map');
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 34.0983425, lng: -118.3267434},
-          zoom: 12
-        });
-         
+        options = {
+          center: myCurrentPosition,
+          zoom: 12,
+        }
+
+        map = new google.maps.Map(element, options);
+
+
+
         infoWindow = new google.maps.InfoWindow;
+        infoWindow.setContent('Location found.');
+        infoWindow.setPosition(myCurrentPosition);
+        infoWindow.open(map);
 
+        console.log(pos);
+      })
+    }
+    else{
+      console.log("you are not online");
+    }
 
-      if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                  var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                  };
-
-                  infoWindow.setPosition(pos);
-                  infoWindow.setContent('Location found.');
-                  infoWindow.open(map);
-                  map.setCenter(pos);
-                }, function() {
-                  handleLocationError(true, infoWindow, map.getCenter());
-                });
-              } else {
-                // Browser doesn't support Geolocation
-                handleLocationError(false, infoWindow, map.getCenter());
-              }
-
-
+      var markers = [];
 
       // Add Submit Function
       $('#submit').click(function(){  
@@ -51,62 +62,64 @@ console.log('testing');
           query = document.getElementById("query").value;
           console.log(location);
           console.log(query);
-     // call the drawmap fuction
-          drawMap(location);
-        })
 
-
-      // add a function Drawmap
-      function drawMap(myCurrentLocation) {      
-          console.log("draw map");  
-          var options = {
-              center: myCurrentLocation,
-              zoom:12,
-          };
-         map = new google.maps.Map(element, options);
-          foursquare();
-        }
-        // end of DrawMap section 
-        //  map = new google.maps.Map(element, options); 
-
-
-
-      function foursquare() {    
           $.ajax({
             type: 'GET',
             dataType: 'jsonp',
             cache: false,
-            url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near='+ location +'&query=' + query,
+            url: 'https://api.foursquare.com/v2/venues/search?client_id=XUXLO0NUOU5C54RCWE2UXF50MHWEL52I1APLQVSMBQJDGFPX&client_secret=0BEDRXQL312RAUAK2M5ON35BLUWSBRNCQDQNKADJ5COXJ4VJ&v=20180212&near='+ location +'&query=' + query,
             success: function(response) {
               console.log(response);
+              //clear the map first
+            clearTheMap();
               response.response.venues.forEach(function(venue) {
                 var venueLatLng = new google.maps.LatLng(venue.location.lat, venue.location.lng);
                 var marker = new google.maps.Marker({
                   map: map,
                   position: venueLatLng,
                 });
+                markers.push(marker);
                 var infowindow = new google.maps.InfoWindow({ 
                   content: venue.name, 
                 });
                 google.maps.event.addListener(marker, 'click', function() {
                   map.setZoom(12);
+                  map.setCenter(marker.position);
                   infowindow.open(map, marker);
                 })
               })
             },
           })
-      }
+     // call the drawmap fuction
+      //    drawMap(location);
+        })
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
 
+      // add a function Drawmap
+      /*function drawMap(myCurrentLocation) {      
+          console.log("draw map");  
+          var options = {
+              center: myCurrentLocation,
+              zoom:12,
+          };
+         map = new google.maps.Map(element, options);
+          map();
+        }
+        // end of DrawMap section 
+        //  map = new google.maps.Map(element, options); 
+*/
+
+
+            function clearTheMap() {
+                  for(var i =0; i < markers.length; i++) {
+                    markers[i].setMap(null);
+                  }
+                    markers = [];
+                }
 
     });
+
+
 
 }) ();
 
