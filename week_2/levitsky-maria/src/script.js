78a855939c56c04e1a54c9c69078ba1eb46f53ce
Graphic <graphic@SOD-SJH-517-27M.local>
2018-03-08 18:23:30 -0500

    merge

--- week_2/levitsky-maria/src/script.js
+++ week_2/levitsky-maria/src/script.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 //tried to convert script in body into seperate js file, would
-=======
-//tried to convert script in body into seperate js file, would
->>>>>>> 2c8e96aa4d206647b62e61be6eaa749669c6d2e1
 //not load map.  
