4783ee640cb43ad0de4d3446c4cba11fda76febc
mlvtsky <mlevitsky@live.com>
2018-03-08 02:40:06 -0500

    Final Project

--- week_5/levitsky-maria/src/script.js
+++ week_5/levitsky-maria/src/script.js
@@ -1,16 +1,20 @@
+//Opening a Google Map Info Window in a seperate Div on mouseover using Foursquare API.
+
 $(document).ready(function(){
 	var element = document.getElementById('foursquare-map')
 	var options = {
     center: new google.maps.LatLng(43.6532, -79.3832),
 		zoom: 13,
 	}
+//opening a google map using foursquare api to display venues	
   var map = new google.maps.Map(element, options)
-	$.ajax({
+	$.ajax({	
 		type: 'GET',
 		datatype: 'jsonp',
 		cache: false,
-		url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=gallery',
-    success: function(response){
+		url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=gallery&limit=25',
+    	success: function(response){
+			
 			console.log(response)
 			response.response.venues.forEach(function(gallery){
 				var venueLatLng = new google.maps.LatLng(gallery.location.lat, gallery.location.lng)
@@ -19,62 +23,41 @@ $(document).ready(function(){
 					position: venueLatLng,
 
 				})
+//if statements for when something appears as undefined will display alternate text
+				if (gallery.location.address === undefined){
+					gallery.location.address = "Address Not Listed";
+				}
+				else {
+					undefined = "Have a nice day";
+				}
+//infowindow and what will be displayed inside it
 				var infowindow = new google.maps.InfoWindow({
-					content: gallery.name
+					content: '<h4>'+ gallery.name +'</h4>' 
 				})
+//addListener event to open infowindow
 				google.maps.event.addListener(marker, 'click', function(){
-					map.setZoom(15);
+					map.setZoom(13);
 					infowindow.open(map, marker);
 				})
+//addListener event to view response fields inside #more-info div, using if statement
+    			google.maps.event.addListener(marker, 'mouseover', function (marker, i) {
+				
+        		if ($('#more-info').css('display') == 'block') {
+           		$('#more-info').css('display', 'none').html("<h2>" + gallery.name + "</h2><br/><p>" + gallery.location.address + "</p>");
+        		} 
+				else {
+            	$('#more-info').css('display', 'block');
+        		}
 
+				}); 
 
-
-				google.maps.event.addListener(infowindow, 'closeclick', function(){
-          map.panTo(this.getPosition());
-					map.setZoom(15);
 				})
-
-
-			})
-		},
+			},
 
 	})
-
+//shows local transit routes
 var transitLayer = new google.maps.TransitLayer();
 transitLayer.setMap(map);
 
 })
 
-
-
-/*
-$(document).ready(function() {
-	var latlng = new google.maps.LatLng('43.6532', '-79.3832');
-	var map = new google.maps.Map(document.getElementById('foursquare-map'), {
-		center: latlng,
-		zoom: 10
-	});
-
-	$.ajax({
-		type: "GET",
-		dataType: "jsonp",
-		cache: false,
-		url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
-		success: function(response) {
-			response.response.venues.forEach(function(venue) {
-				var location = venue.location
-		                var latlng = new google.maps.LatLng(location.lat, location.lng);
-				var marker = new google.maps.Marker({
-					position: latlng,
-					map: map
-				});
-				var infowindow = new google.maps.InfoWindow({ content: venue.name });
-				google.maps.event.addListener(marker, 'click', function() {
-					map.setZoom(12);
-					infowindow.open(map, marker);
-				});
-			});
-		}
-	});
-})
-*/
