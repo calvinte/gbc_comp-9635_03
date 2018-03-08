9d14b80bfae162d18381f2db240418f11046b3a0
Graphic <graphic@SOD-SJH-517-27M.local>
2018-03-08 18:19:27 -0500

    fix repo

--- week_4/levitsky-maria/src/script.js
+++ week_4/levitsky-maria/src/script.js
@@ -2,19 +2,27 @@ $.ajax({
 	type:"GET",
 	dataType: "jsonp",
 	cache: false,
-	url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
+
+	url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=gallery',
+
 	success: function(response){
 
 		response.response.venues.forEach(function(venue){
 			console.log(venue)
 			var location = venue.location
 			var html = '<p>'
+			html += '<h1>'
 			html += venue.name
-			html += '<br />'
+			html == '</h1>'
+			html += '<br/>'
+			html += '<h3>'
+			html += venue.location.address
+			html += '</h3>'
+			html += '<br/>'
 			html += '<img src="'
 			html += 'https://maps.googleapis.com/maps/api/staticmap'
 			html += '?zoom=15'
-			html += '&size=600x450'
+			html += '&size=700x450'
 			html += '&maptype=roadmap'
 			html += '&center=' + location.lat + ',' + location.lng
 			html += '&markers=' + encodeURIComponent('||') + location.lat + ',' + location.lng
@@ -32,33 +40,3 @@ $.ajax({
 
 
 
-
-
-
-/* $.ajax({
-	type: "GET",
-	dataType: "jsonp",
-	cache: false,
-	url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
-	success: function(response) {
-		response.response.venues.forEach(function(venue) {
-			var location = venue.location
-			var html = '';
-			html += '<p>'
-			html += '<strong>'
-			html += venue.name
-			html += '</strong><br />'
-			html += '<img src="https://maps.googleapis.com/maps/api/staticmap'
-			html += '?zoom=15'
-			html += '&size=600x300'
-			html += '&maptype=roadmap'
-                        html += '&center=' + location.lat + ',' + location.lng
-                        html += '&markers=' + encodeURIComponent('||') + location.lat + ',' + location.lng
-			html += '">';
-			html += '</p>'
-			document.body.innerHTML += html;
-		});
-	}
-});
-
-*/
