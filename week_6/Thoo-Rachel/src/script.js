d381bc9e337ac61836cfe20066d1c37627532d28
rwthoo <rwthoo@gmail.com>
2018-03-07 20:43:02 -0500

    Week 6

--- week_6/Thoo-Rachel/src/script.js
+++ week_6/Thoo-Rachel/src/script.js
@@ -1,58 +1,84 @@
+var map;
+var markers = [];
+var lastInfoWindow = null
+
+// initialize the map and listen for any clicks to the search button
 $(document).ready(function() {
-  var latlng = new google.maps.LatLng('43.6532', '-79.3832');
-  var map = new google.maps.Map(document.getElementById('foursquare-map'), {
-    center: latlng,
+  initializeMap();
+
+  $('#map-input button').click(function() {
+    var term = $('#map-input input').val();
+    loadMarkersFromTerm(term);
+  });
+});
+
+// function to initialize the map to be seen on the screen
+function initializeMap() {
+  var toronto = new google.maps.LatLng('43.6532', '-79.3832');
+  map = new google.maps.Map(document.getElementById('foursquare-map'), {
+    center: toronto,
     zoom: 10
   });
+}
+
+// load the markers from the term in the input box
+function loadMarkersFromTerm(term) {
+  var url = 'https://api.foursquare.com/v2/venues/search?client_id=XUXLO0NUOU5C54RCWE2UXF50MHWEL52I1APLQVSMBQJDGFPX&client_secret=0BEDRXQL312RAUAK2M5ON35BLUWSBRNCQDQNKADJ5COXJ4VJ&v=20180212&near=Toronto&query=' + term;
 
   $.ajax({
     type: "GET",
     dataType: "jsonp",
     cache: false,
-    url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
-    success: function(response) {
+    url: url,
+    success: function (response) {
+      clearMap();
       response.response.venues.forEach(function(venue) {
-        var location = venue.location
-                    var latlng = new google.maps.LatLng(location.lat, location.lng);
-        var marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
-        var infowindow = new google.maps.InfoWindow({ content: venue.name });
-        google.maps.event.addListener(marker, 'click', function() {
-          map.setZoom(12);
-          infowindow.open(map, marker);
-        });
+        addVenueMarkerOnMap(venue);
       });
     }
-  });
+  })
+}
+var infowindow = new google.maps.InfoWindow({
+          content: venue.name, 
 })
-$(document).ready(function() {
-  var latlng = new google.maps.LatLng('43.6532', '-79.3832');
-  var map = new google.maps.Map(document.getElementById('foursquare-map'), {
-    center: latlng,
-    zoom: 10
+google.maps.event.addListener(marker,"click", function ClickHandler(){
+          map.setCenter(marker.position)
+          map.setZoom(10)
+          infowindow.open(map, marker)
+          if (lastInfoWindow) {
+            lastInfoWindow.close()
+          }
+          lastInfoWindow = infowindow
+        })
+        
+        google.maps.event.addListener(infowindow,'closeclick', function(){
+          lastInfoWindow = null
+        })
+        
+        google.maps.event.addListener(marker)
+}
+
+// add the marker and its window to the map
+function addVenueMarkerOnMap(venue) {
+  var location = venue.location;
+  var latlng = new google.maps.LatLng(location.lat, location.lng);
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
   });
 
-  $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    cache: false,
-    url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
-    success: function(response) {
-      response.response.venues.forEach(function(venue) {
-        var location = venue.location
-                    var latlng = new google.maps.LatLng(location.lat, location.lng);
-        var marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
-        var infowindow = new google.maps.InfoWindow({ content: venue.name });
-        google.maps.event.addListener(marker, 'click', function() {
-          map.setZoom(12);
-          infowindow.open(map, marker);
-        });
-      });
-    }
+  var infoWindow = new google.maps.InfoWindow({ content: venue.name });
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.open(map, marker);
   });
-})
+
+  markers.push(marker);
+}
+
+// clear map
+function clearMap() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
